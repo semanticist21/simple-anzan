9b9248d341a9c0116c4a5950421cfa6ca23e9b1a
jiwon <semanticist21@github.com>
2023-03-03 23:37:45 +0900

    .

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'simple anzan',
+      home: Home(),
+      themeMode: ThemeMode.dark,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  int _currentPageIndex = 0;
+
+  static final List<Widget> _pages = [];
+
+  void _onTap(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
+        ],
+        currentIndex: _currentPageIndex,
+        onTap: _onTap,
+      ),
+    );
+  }
+}
