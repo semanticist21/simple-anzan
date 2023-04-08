eca08c5ae8bc26cce5be3b1205e3dd3de613e8b4
Jiwon Park <semanticist21@github.com>
2023-04-08 22:29:30 +0900

    default speed 수정

--- lib/src/settings/plus_pref/prefs/speed.dart
+++ lib/src/settings/plus_pref/prefs/speed.dart
@@ -9,7 +9,7 @@ class SpeedPref implements PreferenceInterfaceItems<Speed, Duration> {
   final String _saveCustomKey = 'intervalCustom';
 
   final int _defaultIndex = 2;
-  final int _defaultCustomValue = 100;
+  final int _defaultCustomValue = 500;
 
   late int _currentIndex;
   late Speed _currentValue;
